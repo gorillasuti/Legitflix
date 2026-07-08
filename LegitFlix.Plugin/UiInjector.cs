c31b44dde94c931b76b7c1fb78c0eeaf852e1f7d
gorillasuti <kiralydaniel517@gmail.com>
2026-07-08 19:19:45 +0200

    fix(ui): reposition movie action popover, reset button styles, and enable dialog backdrop close

--- LegitFlix.Plugin/UiInjector.cs
+++ LegitFlix.Plugin/UiInjector.cs
@@ -191,6 +191,7 @@ namespace LegitFlix.Plugin
 
             document.getElementById('lf-btn-action').onclick = function() {
                 dialog.close();
+                dialog.remove();
                 if (isClassic) {
                     document.cookie = 'lf-classic-view=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
                     window.location.href = window.location.origin + window.location.pathname;
@@ -204,10 +205,24 @@ namespace LegitFlix.Plugin
             
             document.getElementById('lf-btn-cancel').onclick = function() {
                 dialog.close();
+                dialog.remove();
                 if (isClassic) {
                     window.location.href = window.location.origin + window.location.pathname + '?classic=true#/home';
                 }
             };
+
+            dialog.addEventListener('click', function(event) {
+                var rect = dialog.getBoundingClientRect();
+                var isInDialog = (rect.top <= event.clientY && event.clientY <= rect.top + rect.height &&
+                                  rect.left <= event.clientX && event.clientX <= rect.left + rect.width);
+                if (!isInDialog) {
+                    dialog.close();
+                    dialog.remove();
+                    if (isClassic) {
+                        window.location.href = window.location.origin + window.location.pathname + '?classic=true#/home';
+                    }
+                }
+            });
 
             dialog.showModal();
         }
